5a86c942f2b0dc08d3eaffc66070f52a3903360f
Momeemt <momiimt@gmail.com>
2020-06-16 18:43:31 +0900

    ✂️ Module分割

--- src/nimaterial.nim
+++ src/nimaterial.nim
@@ -1,32 +1,6 @@
 import tables
-
-type HTML* = enum
-  p
-  mDiv = "div"
-  h1
-  h2
-  a
-
-type Color* = enum
-  red = "#F44336"
-  pink = "#E91E63"
-  purple = "#9C27B0"
-  deepPurple = "#673AB7"
-  indigo = "#3F51B5"
-  blue = "#2196F3"
-  lightBlue = "#03A9F4"
-  cyan = "#00BCD4"
-  teal = "#009688"
-  green = "#4CAF50"
-  lightGreen = "#8BC34A"
-  lime = "#CDDC39"
-  yellow = "#FFEB3B"
-  amber = "#FFC107"
-  orange = "#FF9800"
-  deepOrange = "#FF5722"
-  brown = "#795548"
-  blueGrey = "#607D8B"
-  grey = "#9E9E9E"
+import nimaterial/[color, HTMLElement]
+export color, HTMLElement
 
 type Flex* = enum
   fStart = "start"
